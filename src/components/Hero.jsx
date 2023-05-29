afc16135e2e8ed4df26e9822a61251f67108a0f1
Rhema7776 <rhemachukwumah7@gmail.com>
2023-05-29 21:35:57 +0100

    fourth commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,38 +12,41 @@ import Card2 from '../assets/BG2.png'
 
 const Hero = () => {
   return (
-    <div className="w-full py-[20%] h-screen bg-zinc-200 flex-col justify-between">
-      <div className="grid md:grid-cols-2 max-w-[1600px] m-auto">
+    <div className=" py-[25%]  h-screen px-4  md-container ">
+      <div className="grid w-full md:grid-cols-2 bg-red-500   flex-col justify-between max-w-[1600px] ">
         <div
-          className=" py-20 flex flex-col justify-center
-             md:items-start w-full px-2 py-8 bg-red-200"
+          className="  md:py-10 flex flex-col justify-center
+             md:items-start w-full md:w-[100%] px-2 py-8 bg-red-200 lg:text-start text-center md:text-start"
         >
-          <h1 className=" font-bold  text-3xl md:text-5xl lg:text-7xl">
-            A special credit <br /> card  made for <br /> Developers
+          <h1 className="  font-bold  lg:text-start text-center md:text-start sm:text-3xl text-5xl md:text-6xl lg:text-7xl xl:text-8xl">
+            A special credit card  made for Developers
           </h1>
-          <p className="py-3 text-sm  text-slate-500 ">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.<br/> Non
+          <p className="py-3 text-sm lg:text-3xl  text-slate-500 ">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Non
             dignissimos quo quaerat unde facilis 
           </p>
-          <div className="w-full lg:w-[73%] md:w-[100%] sm:w-[100%] rounded-xl bg-white flex items-center px-2 py-2 w-[200px] sm:w-[400px] lg:w-[500px]">
+          <div className="w-full md:py-2 xl:w-[100%] lg:w-[95%] lg:text-2xl border-slate-600 md:w-[100%] sm:w-[100%] rounded-xl bg-white flex items-center px-2 py-3 w-[200px] sm:w-[400px] lg:w-[500px]">
             <input type="text" className="bg-transparent p-2 w-full focus:outline-none" placeholder="Enter email address " />
             <button className="py-3  px-6 sm:w-[60%]">Get Started</button>
           </div>
           
-          <div className="flex  justify-between w-[100%] py-6 md:w-[70%] bg-blue-500">
-                <div className="flex justify-between bg-red-300 w-[42%]  items-center">
-                  <h1 className="text-4xl md:text-6xl">2943</h1>
-                  <p className="py-3 text-[12px] md:text-sm md:text-lg  text-slate-500">Cards<br />Delivered</p>
-                </div>
-                <div className="flex justify-between  bg-blue-300 w-[50%] items-center">
-                  <h2 className="text-4xl md:text-6xl">$1M+</h2>
-                  <p className="py-3 text-[12px] md:text-sm md:text-lg text-slate-500 ">Transaction<br />Completed</p>
-                </div>
+          <div className="flex  justify-between xs:w-[100%]  sm:w-[60%] w-[80%] md:py-1 py-6 md:w-[100%] lg:w-[86%] xl:w-[85%] bg-blue-500">
+            <div className="flex xs:flex-col sm:flex-row  bg-red-300 w-[42%]  items-center">
+              <h1 className="text-3xl md:text-4xl lg:text-5xl xl:text-6xl mr-2">2943</h1>
+              <p className="py-3 text-[12px] md:text-sm lg:text-md xl:text-lg   text-slate-500">Cards<br />Delivered</p>
+            </div>
+            <div className="flex xs:flex-col sm:flex-row  bg-blue-300 w-[50%] items-center">
+              <h1 className="text-3xl md:text-4xl bg-slate-500 lg:text-5xl xl:text-6xl mr-2">$1M+</h1>
+              <p className="py-3 text-[12px] lg:text-md xl:text-lg  md:text-sm text-slate-500 bg-red-500 ">Transaction<br />Completed</p>
+              {/* md:text-lg */}
+            </div>
           </div>
         </div>
-        <div className="w-full  bg-violet-500 ">    
-          <img className="absolute lg:w-[30%]  w-[70%] ml-[6%]  md:w-[30%] mt-[12%] md:ml-[16%] "  src={Card2} alt="" />
-          <img className=" cursor-pointer lg:w-[30%]  absolute w-[70%] md:w-[30%]  md:mt-[1%] md:ml-[5%]" src={Card1} alt="" />
+        <div className="w-full bg-violet-500  ">    
+         fldvsdcsxz
+          {/* bg-violet-500 */}
+          <img className="absolute md:w-[270px] md:mt-[200px] xl:mt-[200px] xl:w-[25%] lg:w-[35%] lg:h-[350px] lg:mt-[200px] xl:h-[400px] md:h-[280px] w-[70%] md:ml-[10%] ml-[6%]  md:w-[40%] mt-[12%] "  src={Card2} alt="" />
+          <img className=" cursor-pointer md:w-[270px] md:mt-5 xl:w-[25%] lg:w-[35%] lg:h-[350px] xl:h-[400px] md:h-[300px]  absolute w-[70%] md:w-[40%]  md:mt-[1%] " src={Card1} alt="" />
         </div>
         
       </div>
